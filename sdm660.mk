c17a212d2e879a90145d0c56a1ddee69122d6b6a
Aryan Gupta <guptaaryan189@gmail.com>
2020-04-15 16:53:16 +0000

    sdm660: build android.hardware.camera.common@1.0
     E/ims_camera_jni(2657): Error loading library lib-imscamera.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found

--- sdm660.mk
+++ sdm660.mk
@@ -128,7 +128,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+   android.hardware.camera.common@1.0
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
